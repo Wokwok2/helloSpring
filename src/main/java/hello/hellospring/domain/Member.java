ab9560f687a9887790267d76b412d04c1c51376b
Wokwok2 <hws0602@naver.com>
2022-11-02 13:33:43 +0900

    11.1 강[D[D의

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,15 +1,14 @@
 package hello.hellospring.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
 
     public Long getId() {
         return id;
